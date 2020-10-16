5ce21c2f9270b00c9a8a8fa7ca726d24677f628a
Leomar Linhares <leomar_linhares@hotmail.com>
2020-10-16 01:16:29 -0300

    Create Back-end

---
+++ src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-22.7585364",
+        lng: "-43.4540157",
+        name: "Lar das Meninas",
+        description: "Presta assistência kct",
+        images: [
+            "https://images.unsplash.com/photo-1594750018712-77643025beb6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1576883600124-64c5aa68b4bc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1598137203989-3152bec01cf4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "instruções legais",
+        opening_hours: "Horários de visitas das 18h até 8h",
+        open_on_weekends: "1"
+    },
+
+    {
+        id: 2,
+        lat: "-22.7535364",
+        lng: "-43.4640157",
+        name: "Lar dos Meninos",
+        description: "Presta assistência kct",
+        images: [
+            "https://images.unsplash.com/photo-1594750018712-77643025beb6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1576883600124-64c5aa68b4bc?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1598137203989-3152bec01cf4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "instruções legais",
+        opening_hours: "Horários de visitas das 18h até 8h",
+        open_on_weekends: "0"
+    }
+]
